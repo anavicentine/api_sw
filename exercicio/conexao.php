ebd939bd5bf57ddd6f33ac45585ed30304d307b8
Ana Laura Vicentine Ribeiro <100931526+anavicentine@users.noreply.github.com>
2024-08-06 09:48:26 -0300

    Add files via upload

--- exercicio/conexao.php
+++ exercicio/conexao.php
@@ -6,10 +6,10 @@
 
     $conexao = new mysqli($host,$usuario,$senha,$banco);
 
-    if ($conexao->connect_error){
+    if($conexao->connect_error){
         die('Falha de conexão: ' . $conexao->connect_error);
     }
-    //else{
-    //    echo "CONECTADO COM SUCESSO!";
-    //}
+    // else{
+    //     echo "CONECTADO COM SUCESSO!";
+    // }
 ?>
